ee81ccf9e35413df4f33ce054b1195ed0b8dea58
Aayush Kumar <aaykmr@gmail.com>
2022-06-16 22:29:56 +0530

    Class Componemnt

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,41 +1,58 @@
-import React from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { setUser, updateUser, setFilter } from "../redux/action";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+    console.log(props);
+    this.credit = 0;
+    this.debit = 0;
+    this.total = 0;
+  }
 
-const Dashboard = () => {
-  const dispatch = useDispatch();
-  const user = useSelector((state) => state.users);
-  let debit = 0;
-  let credit = 0;
-  let total = 0;
-  user.transactions.forEach((ele) => {
-    switch (ele.type) {
-      case "debit":
-        debit += ele.amount;
-        total -= ele.amount;
-        break;
-      case "credit":
-        credit += ele.amount;
-        total += ele.amount;
-        break;
-    }
-  });
-  return (
-    <div className="row p-4">
-      <div className="card col d-flex flex-column justify-content-center align-items-center bg-transparent border-success p-4 m-2">
-        <h3>Credit</h3>
-        <h2 className="text-success">{credit}</h2>
-      </div>
-      <div className="card col d-flex flex-column justify-content-center align-items-center bg-transparent border-danger p-4 m-2">
-        <h3>Debit</h3>
-        <h2 className="text-danger">{debit}</h2>
-      </div>
-      <div className="card col d-flex flex-column justify-content-center align-items-center bg-transparent border-primary p-4 m-2">
-        <h3>Balance</h3>
-        <h2 className="text-primary">{total}</h2>
-      </div>
-    </div>
-  );
-};
+  componentDidUpdate() {
+    console.log("Update");
+  }
+  calculate() {
+    this.credit = 0;
+    this.debit = 0;
+    this.total = 0;
+    this.props.user.transactions.forEach((ele) => {
+      switch (ele.type) {
+        case "debit":
+          this.debit += ele.amount;
+          this.total -= ele.amount;
+          break;
+        case "credit":
+          this.credit += ele.amount;
+          this.total += ele.amount;
+          break;
+      }
+    });
+  }
 
-export default Dashboard;
+  render() {
+    this.calculate();
+    console.log("first");
+    return (
+      <div className="row p-4">
+        <div className="card col d-flex flex-column justify-content-center align-items-center bg-transparent border-success p-4 m-2">
+          <h3>Credit</h3>
+          <h2 className="text-success">{this.credit}</h2>
+        </div>
+        <div className="card col d-flex flex-column justify-content-center align-items-center bg-transparent border-danger p-4 m-2">
+          <h3>Debit</h3>
+          <h2 className="text-danger">{this.debit}</h2>
+        </div>
+        <div className="card col d-flex flex-column justify-content-center align-items-center bg-transparent border-primary p-4 m-2">
+          <h3>Balance</h3>
+          <h2 className="text-primary">{this.total}</h2>
+        </div>
+      </div>
+    );
+  }
+}
+const mapStateToProps = (state) => ({
+  user: state.users,
+});
+
+export default connect(mapStateToProps)(Dashboard);
--- src/route/Routes.js
+++ src/route/Routes.js
@@ -1,11 +1,5 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-// import Contact from "./Contact";
-// import Login from "./Login";
-// import User from "./Name";
-// import Navbar from "./Navbar";
-// import Price from "./Price";
-// import Signup from "./Signup";
 import Home from "../components/Home";
 
 const Routee = () => {
@@ -14,11 +8,6 @@ const Routee = () => {
       <>
         <Routes>
           <Route path="/" element={<Home />} />
-          {/* <Route path="/price" element={<Price />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/Signup" element={<Signup />} />
-          <Route path="/user/:id" element={<User />}></Route> */}
         </Routes>
       </>
     </div>
