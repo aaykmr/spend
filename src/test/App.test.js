8434b43d52f4c4a6f46bbf4ed1bc8e1b2e69b5a5
Aayush Kumar <aaykmr@gmail.com>
2022-06-24 15:04:24 +0530

    Test fix App.js

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,10 +1,8 @@
 import React from "react";
-import { fireEvent, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import App from "../App";
-import { Login } from "../components/Login";
 import { Provider } from "react-redux";
 import { store } from "../redux/store/store";
-import userEvent from "@testing-library/user-event";
 import { BrowserRouter } from "react-router-dom";
 beforeEach(() => {
   render(
