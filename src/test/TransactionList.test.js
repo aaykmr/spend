b0b5e00c5e35cb83aa80c546240b057cadf07de7
Aayush Kumar <aaykmr@gmail.com>
2022-06-27 15:07:01 +0530

    Transaction branch coverage test

--- src/test/TransactionList.test.js
+++ src/test/TransactionList.test.js
@@ -5,27 +5,30 @@ import { store } from "../redux/store/store";
 import json from "../users.json";
 import TransactionList from "../components/TransactionList";
 
-beforeEach(() => {
-  render(
-    <Provider store={store}>
-      <TransactionList user={json[0]} type={"all"} />
-    </Provider>
-  );
-});
-describe("Testing the Login/Sigup page", () => {
-  it("Renders Transaction list Fields  ", () => {
-    const transactions = screen.getAllByTestId("transaction");
-    expect(transactions.length).toBeGreaterThan(0);
+let arr = ["all", "debit", "credit"];
+arr.forEach((ele) => {
+  beforeEach(() => {
+    render(
+      <Provider store={store}>
+        <TransactionList user={json[0]} type={ele} />
+      </Provider>
+    );
   });
-  it("Renders Delete transaction button", () => {
-    const deleteTransactions = screen.getAllByTestId("transaction");
-    expect(deleteTransactions.length).toBeGreaterThan(0);
-  });
-  it("Delete transaction button works", () => {
-    let deleteTransactions = screen.getAllByTestId("deleteTransaction");
-    const initialLength = deleteTransactions.length;
-    fireEvent.click(deleteTransactions[0]);
-    deleteTransactions = screen.getAllByTestId("deleteTransaction");
-    expect(deleteTransactions.length).toBe(initialLength - 1);
+  describe("Testing the Transaction List page", () => {
+    it("Renders Transaction list Fields  ", () => {
+      const transactions = screen.getAllByTestId("transaction");
+      expect(transactions.length).toBeGreaterThan(0);
+    });
+    it("Renders Delete transaction button", () => {
+      const deleteTransactions = screen.getAllByTestId("transaction");
+      expect(deleteTransactions.length).toBeGreaterThan(0);
+    });
+    it("Delete transaction button works", () => {
+      let deleteTransactions = screen.getAllByTestId("deleteTransaction");
+      const initialLength = deleteTransactions.length;
+      fireEvent.click(deleteTransactions[0]);
+      deleteTransactions = screen.getAllByTestId("deleteTransaction");
+      expect(deleteTransactions.length).toBe(initialLength - 1);
+    });
   });
 });
